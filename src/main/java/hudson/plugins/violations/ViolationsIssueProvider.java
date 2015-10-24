e5e9239b1124796a816a368d7d85cf8e70829907
Michael Prankl <eidottermihi@gmail.com>
2015-10-24 23:58:23 +0200

    Only depend on Provider module.

--- src/main/java/hudson/plugins/violations/ViolationsIssueProvider.java
+++ src/main/java/hudson/plugins/violations/ViolationsIssueProvider.java
@@ -9,7 +9,7 @@ import hudson.plugins.violations.model.FileModel;
 import hudson.plugins.violations.model.Violation;
 import hudson.plugins.violations.render.FileModelProxy;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
-import org.jenkinsci.plugins.codehealth.model.Priority;
+import org.jenkinsci.plugins.codehealth.provider.Priority;
 import org.jenkinsci.plugins.codehealth.provider.issues.Issue;
 import org.jenkinsci.plugins.codehealth.provider.issues.IssueProvider;
 
